c69e39498549de25361c3c08893c6c38226f907d
Milan Verma <millan.verma@gmail.com>
2021-01-09 20:12:50 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import restaurent from "./restaurent.jpg";
+
+function Header(props){
+  console.log(props);
+  return(
+    <header>
+      <h1>{props.name}'s Restaurent</h1>
+    </header>
+  );
+}
+
+
+
+function Main(props){  
+  return(    
+    <section>
+      <p>We serve hot and spicy dishes to make your day {props.adjective}.</p>
+      <img src={restaurent} height={235} alt="inside the restaurent" />
+      <ul style={{textAlign: "left"}}>
+        {dishes.map((dishes) => (
+        <li key={dishes}>{dishes}</li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+function Footer(props){
+  return(
+    <footer>
+      <p>Copyright {props.year} </p>
+    </footer>
+  );
+}
+
+
+const dishes = [
+  "French Fries",
+  "Maha Veg Burger",
+  "Duggal's Butter Chicken",
+  "Pandit's Burger",
+  "Bombay Bhelpuri",
+  "Chole Bhature"
+]
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Milan" dishes={dishes} />
+      <Main adjective="awesome" />
+      <Footer year={new Date().getFullYear()} />
     </div>
   );
 }
