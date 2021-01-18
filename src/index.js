04967f30f79940f0ae0ce99c2756069d6ef80619
Milan Verma <millan.verma@gmail.com>
2021-01-17 19:36:20 -0500

    fetch api github

--- src/index.js
+++ src/index.js
@@ -3,11 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from "./App"
 
-const [, , third] = ["boots", "tent", "headlamp"];
-console.log(third);
 
-//Authorized components only
 ReactDOM.render(
-  <App />,
+  <App login='millanair'/>,
   document.getElementById('root')
 );
